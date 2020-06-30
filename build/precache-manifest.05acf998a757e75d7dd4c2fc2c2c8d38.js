0c8188cad617ae42c44238d3f4979089dab8b3f1
plotnikovvlad94 <plotnikov.va94@gmail.com>
2020-06-30 20:17:49 +0300

    gh-pages

---
+++ build/precache-manifest.05acf998a757e75d7dd4c2fc2c2c8d38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cc49749f044a2e9544be29cf8f81f49",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d646e97a7800a323467",
+    "url": "/static/css/main.423c6f5e.chunk.css"
+  },
+  {
+    "revision": "8ac36b07386836341922",
+    "url": "/static/js/2.f7c01d1f.chunk.js"
+  },
+  {
+    "revision": "fae6e97322ddc0579046aa829b382727",
+    "url": "/static/js/2.f7c01d1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d646e97a7800a323467",
+    "url": "/static/js/main.7a2a80cd.chunk.js"
+  },
+  {
+    "revision": "0a6d902538761987f488",
+    "url": "/static/js/runtime-main.a2ebebe9.js"
+  }
+]);
